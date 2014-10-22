16ddf379fbb133b34751deaeb70eda4fa5bd7f3a
Dave Charnuska <davecharnuska@gmail.com>
2014-10-22 14:56:03 -0400

    complete contact page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,15 +25,35 @@ $("#contact-target").on("click", function(event) {
   $(".form-group").removeClass("alert alert-success");
   $(".form-group").removeAttr("role","alert");
 
-  if( !$(".form-control").val() ) {
-    $(this).parent().addClass("has-error");
-    $(this).parent().addClass("alert alert-danger");
-    $(this).parent().attr("role","alert");
+  if( !$(".form-control-name").val() ){
+    $(".form-group-name").addClass("has-error");
+    $(".form-group-name").addClass("alert alert-danger");
   } else {
-    $(this).parent().addClass("has-success");
-    $(this).parent().addClass("alert alert-success");
-    $(this).parent().attr("role","alert");
+    $(".form-group-name").addClass("has-success");
+    $(".form-group-name").addClass("alert alert-success");
   }
+
+  $(".form-group-name").attr("role","alert");
+
+  if( !$(".form-control-email").val() ){
+    $(".form-group-email").addClass("has-error");
+    $(".form-group-email").addClass("alert alert-danger");
+  } else {
+    $(".form-group-email").addClass("has-success");
+    $(".form-group-email").addClass("alert alert-success");
+  }
+
+  $(".form-group-email").attr("role","alert");
+
+  // if( !$(".form-control-name").val() || !$(".form-control-email").val() ) {
+  //   $(this).parent().addClass("has-error");
+  //   $(this).parent().addClass("alert alert-danger");
+  //   $(this).parent().attr("role","alert");
+  // } else {
+  //   $(this).parent().addClass("has-success");
+  //   $(this).parent().addClass("alert alert-success");
+  //   $(this).parent().attr("role","alert");
+  // }
 
 });
 
