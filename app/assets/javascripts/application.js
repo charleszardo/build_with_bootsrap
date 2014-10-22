687598e654232f4d2f57cc0c60d320e4638e26a4
Dave Charnuska <davecharnuska@gmail.com>
2014-10-22 16:30:29 -0400

    add tooltips

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,45 +16,48 @@
 //= require bootstrap
 
 $(document).ready(function(){
-$("#contact-target").on("click", function(event) {
-  event.preventDefault();
-  //start by clearing any validation classes from inputs
-  $(".form-group").removeClass("has-error");
-  $(".form-group").removeClass("has-success");
-  $(".form-group").removeClass("alert alert-danger");
-  $(".form-group").removeClass("alert alert-success");
-  $(".form-group").removeAttr("role","alert");
 
-  if( !$(".form-control-name").val() ){
-    $(".form-group-name").addClass("has-error");
-    $(".form-group-name").addClass("alert alert-danger");
-  } else {
-    $(".form-group-name").addClass("has-success");
-    $(".form-group-name").addClass("alert alert-success");
-  }
+  $(".mySelector").tooltip();
 
-  $(".form-group-name").attr("role","alert");
+  $("#contact-target").on("click", function(event) {
+    event.preventDefault();
+    //start by clearing any validation classes from inputs
+    $(".form-group").removeClass("has-error");
+    $(".form-group").removeClass("has-success");
+    $(".form-group").removeClass("alert alert-danger");
+    $(".form-group").removeClass("alert alert-success");
+    $(".form-group").removeAttr("role","alert");
 
-  if( !$(".form-control-email").val() ){
-    $(".form-group-email").addClass("has-error");
-    $(".form-group-email").addClass("alert alert-danger");
-  } else {
-    $(".form-group-email").addClass("has-success");
-    $(".form-group-email").addClass("alert alert-success");
-  }
+    if( !$(".form-control-name").val() ){
+      $(".form-group-name").addClass("has-error");
+      $(".form-group-name").addClass("alert alert-danger");
+    } else {
+      $(".form-group-name").addClass("has-success");
+      $(".form-group-name").addClass("alert alert-success");
+    }
 
-  $(".form-group-email").attr("role","alert");
+    $(".form-group-name").attr("role","alert");
 
-  // if( !$(".form-control-name").val() || !$(".form-control-email").val() ) {
-  //   $(this).parent().addClass("has-error");
-  //   $(this).parent().addClass("alert alert-danger");
-  //   $(this).parent().attr("role","alert");
-  // } else {
-  //   $(this).parent().addClass("has-success");
-  //   $(this).parent().addClass("alert alert-success");
-  //   $(this).parent().attr("role","alert");
-  // }
+    if( !$(".form-control-email").val() ){
+      $(".form-group-email").addClass("has-error");
+      $(".form-group-email").addClass("alert alert-danger");
+    } else {
+      $(".form-group-email").addClass("has-success");
+      $(".form-group-email").addClass("alert alert-success");
+    }
 
-});
+    $(".form-group-email").attr("role","alert");
+
+    // if( !$(".form-control-name").val() || !$(".form-control-email").val() ) {
+    //   $(this).parent().addClass("has-error");
+    //   $(this).parent().addClass("alert alert-danger");
+    //   $(this).parent().attr("role","alert");
+    // } else {
+    //   $(this).parent().addClass("has-success");
+    //   $(this).parent().addClass("alert alert-success");
+    //   $(this).parent().attr("role","alert");
+    // }
+
+  });
 
 });
